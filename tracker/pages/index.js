90953feec1fd76587396fe55bc79de197914a509
Saul <saul.vg1@gmail.com>
2021-01-16 05:18:06 -0700

    finish section 7/8: apollo client/ apollo client part 2 ssr

--- tracker/pages/index.js
+++ tracker/pages/index.js
@@ -1,14 +1,24 @@
 import styles from '../styles/Home.module.css';
+import { withApollo } from '../lib/apollo.js';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 //component imports
 import Layout from '../components/Layout.js';
 
-export default function Home() {
+const HELLO_QUERY = gql`
+  query helloQuery {
+    sayHello
+  }
+`;
+
+const Home = () => {
+  const { data, loading, error } = useQuery(HELLO_QUERY);
+  if (loading) return <div />;
+
   return (
     <Layout className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{data.sayHello}</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -58,4 +68,5 @@ export default function Home() {
       </footer>
     </Layout>
   );
-}
+};
+export default withApollo(Home);
