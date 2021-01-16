ed0bfb6d8d2371f38c6233a1e0dcfbefa5c51891
Saul Vega <saulv@SaulsMacBook-Pro.local>
2021-01-16 05:10:07 -0700

    finish section 7/8: apollo client/ apollo client part 2 ssr

--- tracker/pages/api/graphql.js
+++ tracker/pages/api/graphql.js
@@ -1,16 +1,42 @@
-//short way
-export default (req, res) => {
-  res.status(200).json({
-    test: 'hello levelup',
-  });
+import { ApolloServer, gql } from 'apollo-server-micro';
+
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHello: () => {
+      return 'Hello level up';
+    },
+  },
 };
-//long way
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default apolloServer.createHandler({ path: '/api/graphql' });
+
+// //short way
 // export default (req, res) => {
-//   res.setHeader('Content-Type', 'application/json');
-//   res.statusCode = 200;
-//   res.end(
-//     JSON.stringify({
-//       test: 'hello levelup',
-//     })
-//   );
+//   res.status(200).json({
+//     test: 'hello levelup',
+//   });
 // };
+// //long way
+// // export default (req, res) => {
+// //   res.setHeader('Content-Type', 'application/json');
+// //   res.statusCode = 200;
+// //   res.end(
+// //     JSON.stringify({
+// //       test: 'hello levelup',
+// //     })
+// //   );
+// // };
